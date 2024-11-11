af56e7c8803ec13eb4be594ccec14d10416854cc
Jonathan Wight <schwa@schwa.io>
2024-11-11 09:45:47 -0800

    Add initial macro support.

--- Package.swift
+++ Package.swift
@@ -1,29 +1,44 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
     name: "NotSwiftUI",
     platforms: [.macOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NotSwiftUI",
-            targets: ["NotSwiftUI"])
+            targets: ["NotSwiftUI"]
+        ),
+        .executable(
+            name: "NotSwiftUIClient",
+            targets: ["NotSwiftUIClient"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/swiftlang/swift-syntax.git",
+            from: "600.0.0-latest")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NotSwiftUI",
-            dependencies: []),
+            dependencies: ["NotSwiftUIMacros"]
+        ),
+        .macro(
+            name: "NotSwiftUIMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        .executableTarget(
+            name: "NotSwiftUIClient", dependencies: ["NotSwiftUI"]
+        ),
         .testTarget(
             name: "NotSwiftUITests",
-            dependencies: ["NotSwiftUI"])
+            dependencies: ["NotSwiftUI"]
+        )
     ]
 )
