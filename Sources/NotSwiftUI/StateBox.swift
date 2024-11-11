3d67674689aca12abb989ae1d9a9822c1f278d03
Jonathan Wight <jwight@mac.com>
2024-11-10 22:00:13 -0800

    Clean-up/organise statebox.

--- Sources/NotSwiftUI/StateBox.swift
+++ Sources/NotSwiftUI/StateBox.swift
@@ -1,7 +1,33 @@
 internal final class StateBox<Wrapped> {
-    private weak var graph: Graph?
     private var _value: Wrapped
+    private weak var _graph: Graph?
     private var dependencies: [WeakBox<Node>] = []
+
+    var graph: Graph? {
+        if _graph == nil {
+            _graph = Graph.current
+            assert(_graph != nil, "StateBox must be used within a Graph.")
+        }
+        return _graph
+    }
+
+    var wrappedValue: Wrapped {
+        get {
+            // Remove lazy values whose nodes have been deallocated
+            dependencies = dependencies.filter { $0.wrappedValue != nil }
+
+            let currentNode = graph?.activeNodeStack.last
+            if let currentNode, !dependencies.contains(where: { $0() === currentNode }) {
+                dependencies.append(WeakBox(currentNode))
+            }
+            return _value
+        }
+        set {
+            _value = newValue
+            valueDidChange()
+        }
+    }
+
     var binding: Binding<Wrapped> = Binding(
         get: { fatalError("Empty Binding: get() called.") },
         set: { _ in fatalError("Empty Binding: set() called.") }
@@ -16,27 +42,8 @@ internal final class StateBox<Wrapped> {
         })
     }
 
-    var wrappedValue: Wrapped {
-        get {
-            if graph == nil {
-                graph = Graph.current
-            }
-            // Remove lazy values whose nodes have been deallocated
-            dependencies = dependencies.filter { $0.wrappedValue != nil }
-            guard let graph else {
-                fatalError("StateBox used outside of Graph")
-            }
-            if let node = graph.activeNodeStack.last, dependencies.contains(where: { $0.wrappedValue === node }) == false {
-                dependencies.append(WeakBox(node))
-            }
-            return _value
-        }
-        set {
-            if graph == nil {
-                graph = Graph.current
-            }
-            _value = newValue
-            dependencies.compactMap { $0() }.forEach { $0.setNeedsRebuild() }
-        }
+    /// Update dependencies when the value changes
+    func valueDidChange() {
+        dependencies.forEach { $0()?.setNeedsRebuild() }
     }
 }
